436f7e918d5f5f6e85b75fb7b9d785e6c65b2a35
User <User@DESKTOP-6ND7VHE>
2024-03-20 18:52:49 +0200

    Добавьте файлы проекта.

---
+++ FindWord/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Write("Word to search: ");
+        string word = Console.ReadLine();
+
+        Console.Write("Path to the file: ");
+        string filePath = Console.ReadLine();
+
+        int timesCount = await FindWordInFileAsync(word, filePath);
+
+        if (timesCount == -1)
+            Console.WriteLine("File not found.");
+        else
+            Console.WriteLine($"The word '{word}' is found {timesCount} times in the file.");
+    }
+
+    static async Task<int> FindWordInFileAsync(string word, string filePath)
+    {
+        try
+        {
+            string textContent = await File.ReadAllTextAsync(filePath);
+
+            int searchedWordCount = textContent
+                .Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                .Count(w => w.Equals(word, StringComparison.OrdinalIgnoreCase));
+            return searchedWordCount;
+        }
+        catch (FileNotFoundException)
+        {
+            return -1;
+        }
+    }
+}
+
